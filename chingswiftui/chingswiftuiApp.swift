c4c8f62b586f819d210bcce23e7d7a49794bcbae
valentineejk <valentineejk@gmail.com>
2022-05-13 10:55:06 +0100

    Initial Commit

---
+++ chingswiftui/chingswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  chingswiftuiApp.swift
+//  chingswiftui
+//
+//  Created by Valentineejk on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct chingswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
